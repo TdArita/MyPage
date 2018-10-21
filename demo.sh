262d0a872634d75127b48d79ad88aa91d5e996f8
TdArita <tang601619508@gmail.com>
2018-10-21 22:09:40 +0800

    demosh

--- demo.sh
+++ demo.sh
@@ -6,12 +6,12 @@ else
   cd $1
   mkdir css js
   touch index.html css/style.css js/main.js
-  echo "<!DOCTYPE> <title>Hello</title> <h1>Hi</h1>" >> index.html
+  echo "<!DOCTYPE> <title>Hello</title> <h1>Hi</h1>" > index.html
   cd ./css
-  echo "h1{color: red;}" >> style.css
+  echo "h1{color: red;}" > style.css
   cd ../js
   echo "var string = 'Hello World'
- alert(string)" >> main.js
+ alert(string)" > main.js
   echo 'success'
   exit
 fi
