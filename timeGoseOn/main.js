f6d2cfa6297ff0341396606ad29c5f121a9d9d0e
TdArita <tang601619508@gmail.com>
2018-12-06 21:18:13 +0800

    time goes on

---
+++ timeGoseOn/main.js
@@ -0,0 +1,11 @@
+setInterval(function() {
+	var a = new Date()
+	var b = a.getTime()
+	var c = (b - Date.parse('01 Jan 2018 00:00:00 GMT')) / ( Date.parse('01 Jan 2019 00:00:00 GMT') - Date.parse('01 Jan 2018 00:00:00 GMT'))
+	c = Math.floor(c*100000000)
+	c = parseInt(c)
+	console.log(c)
+	var d = document.getElementById('time')
+	var e = a.toLocaleString()
+	d.innerHTML = "现在是" + e + "</br></br></br>" + "这一年已过去" + c/1000000 + "%"
+}, 100)
