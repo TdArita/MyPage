f770c7ded72a27406c7bf25e8d7f94b7171c4bbb
TdArita <tang601619508@gmail.com>
2019-03-12 23:58:41 +0800

    finish

--- canvas-1/main.js
+++ canvas-1/main.js
@@ -1,15 +1,42 @@
 
+// var eraserEnabled=false
+// eraser.onclick=function(){
+//     eraserEnabled=true
+//     actions.className='actions x'
+// }
+// painter.onclick=function () {
+//     eraserEnabled=false
+//     actions.className='actions'
+// }
+
 var eraserEnabled=false
+pen.onclick=function(){
+    eraserEnabled=false
+    pen.classList.add('active')
+    eraser.classList.remove('active')
+}
 eraser.onclick=function(){
     eraserEnabled=true
-    actions.className='actions x'
+    eraser.classList.add('active')
+    pen.classList.remove('active')
 }
-painter.onclick=function () {
-    eraserEnabled=false
-    actions.className='actions'
+clear.onclick=function(){
+    context.clearRect(0, 0, myCanvas.width, myCanvas.height)
 }
+save.onclick=function(){
+    var url=myCanvas.toDataURL("image/png")
+    console.log(url)
+    var a=document.createElement('a')
+    document.body.appendChild(a)
+    a.href=url
+    a.download='随手一画'
+    a.target='_blank'
+    a.click()
+}
+
 var myCanvas=document.getElementById('canvasArea')
 var context=myCanvas.getContext('2d')
+var lineWidth = 5
 
 autoSetCanvasSize(myCanvas)
 listenToUser(myCanvas)
@@ -25,15 +52,56 @@ function autoSetCanvasSize(canvas){
         canvas.height = pageHeight
     }
 }
-
+function drawBackground(){
+}
 function drawLine(x1, y1, x2, y2){
     context.beginPath()
-    context.strokeStyle='black'
+    // context.strokeStyle='black'
     context.moveTo(x1,y1)
-    context.lineWidth=5
+    context.lineWidth=lineWidth
     context.lineTo(x2,y2)
     context.stroke()
     context.closePath()
+}
+
+black.onclick=function () {
+    context.fillStyle='black'
+    context.strokeStyle='black'
+    black.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+    red.classList.remove('active')
+}
+red.onclick=function () {
+    context.fillStyle='red'
+    context.strokeStyle='red'
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+    black.classList.remove('active')
+}
+green.onclick=function () {
+    context.fillStyle='green'
+    context.strokeStyle='green'
+    green.classList.add('active')
+    blue.classList.remove('active')
+    red.classList.remove('active')
+    black.classList.remove('active')
+}
+blue.onclick=function () {
+    context.fillStyle='blue'
+    context.strokeStyle='blue'
+    blue.classList.add('active')
+    red.classList.remove('active')
+    green.classList.remove('active')
+    black.classList.remove('active')
+}
+
+thin.onclick=function(){
+    lineWidth=5
+}
+thick.onclick=function(){
+    lineWidth=10
 }
 
 function listenToUser(canvas){
