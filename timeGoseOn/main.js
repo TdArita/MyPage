7a75bfc79a14b565f7a4bc9191157e7c35ccda05
TdArita <tang601619508@gmail.com>
2019-08-15 20:37:54 +0800

    add-progressBar

--- timeGoseOn/main.js
+++ timeGoseOn/main.js
@@ -1,4 +1,5 @@
 setInterval(function() {
+	var progress = document.querySelector('.progress')
 	var a = new Date()
 	var b = a.getTime()
 	var c = (b - Date.parse('01 Jan 2019 00:00:00 GMT')) / ( Date.parse('01 Jan 2020 00:00:00 GMT') - Date.parse('01 Jan 2019 00:00:00 GMT'))
@@ -8,4 +9,5 @@ setInterval(function() {
 	var d = document.getElementById('time')
 	var e = a.toLocaleString()
 	d.innerHTML = "</br></br></br>" + "现在是" + e + "</br></br></br>" + "这一年已过去" + c/1000000 + "%" + "</br></br></br>" 
+	progress.style.width = c/1000000 + "%"
 }, 100)
